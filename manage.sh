ca2058d60b088441499186cc52dbd894aec07a55
Nicholas Rempel <nbrempel@gmail.com>
2017-11-02 12:38:31 -0700

    init with docker-compose environment to run nodes and client

---
+++ manage.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_HOME="$( cd "$( dirname "$0" )" && pwd )"
+cd $SCRIPT_HOME
+
+case "$1" in
+  start_nodes)
+      docker-compose up node1 node2 node3 node4
+    ;;
+  start_client)
+      docker-compose run client
+    ;;
+  stop)
+      docker-compose stop
+    ;;
+  build)
+      docker-compose build
+    ;;
+  rebuild)
+      docker-compose build --no-cache
+    ;;
+  *)
+    echo $"Usage: $0 {start_nodes|start_client|stop|build|rebuild}"
+    RETVAL=1
+esac
+
+cd - > /dev/null
